4545b65fb9777383998229725eb2f4b23783d463
haruaki11052 <haruaki@gem-project.shop>
2022-07-31 16:05:51 +0900

    HelloWorld-dapp Add infura.io Setting

--- HelloWorld-dapp/client/src/getWeb3.js
+++ HelloWorld-dapp/client/src/getWeb3.js
@@ -6,14 +6,39 @@ const getWeb3 = () =>
     window.addEventListener("load", async () => {
       // Modern dapp browsers...
       if (window.ethereum) {
-        const web3 = new Web3(window.ethereum);
-        try {
-          // Request account access if needed
-          await window.ethereum.enable();
-          // Accounts now exposed
+        const Chains = {
+          1: "Mainnet",
+          3: "Ropsten",
+          4: "Rinkeby",
+          42: "Kovan",
+          1337: "Geth private chain(default )",
+          61: "Ethereum Classic Mainnet",
+          62: "Morden",
+        };
+
+        const hexChainId = await window.ethereum.request({
+          method: "eth_chainId",
+        });
+        const chainId = parseInt(hexChainId);
+        const name = Chains[chainId];
+
+        if (chainId === 3) {
+          const { REACT_APP_INFURA_API_URL, REACT_APP_MNEMONIC } = process.env;
+          const HDWalletProvider = require("@truffle/hdwallet-provider");
+          const web3 = new Web3(
+            new HDWalletProvider(REACT_APP_MNEMONIC, REACT_APP_INFURA_API_URL)
+          );
           resolve(web3);
-        } catch (error) {
-          reject(error);
+        } else {
+          const web3 = new Web3(window.ethereum);
+          try {
+            // Request account access if needed
+            await window.ethereum.enable();
+            // Accounts now exposed
+            resolve(web3);
+          } catch (error) {
+            reject(error);
+          }
         }
       }
       // Legacy dapp browsers...
