b1e14f3fd168cd1b374379ba4b349315f32ab01d
haruaki11052 <haruaki@gem-project.shop>
2022-08-05 19:33:08 +0900

    Add infura.io Setting

--- SimpleStorage-dapp/truffle-config.js
+++ SimpleStorage-dapp/truffle-config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "client/.env") });
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { REACT_APP_INFURA_API_URL, REACT_APP_MNEMONIC } = process.env;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -6,13 +9,19 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
-      port: 8545
-    }
+      port: 8545,
+    },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(REACT_APP_MNEMONIC, REACT_APP_INFURA_API_URL),
+      network_id: 3,
+      gas: 5500000,
+    },
   },
   // Configure your compilers
   compilers: {
     solc: {
-      version: "^0.8.13",      // Fetch exact version from solc-bin (default: truffle's version)
+      version: "^0.8.13", // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
@@ -21,6 +30,6 @@ module.exports = {
       //  },
       //  evmVersion: "byzantium"
       // }
-    }
+    },
   },
 };
